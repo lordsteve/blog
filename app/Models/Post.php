a02839fc539ee95ff0047671a3bf77a9c48dec77
Steve Beaudry <live.remix@gmail.com>
2021-11-26 16:54:35 -0500

    Learning through section 2

---
+++ app/Models/Post.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+
+class Post
+{
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt; 
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function all()  //Begin gathering together all posts 
+    {
+        return cache()->rememberForever('posts.all', function () {   //Save all of the following on client-side cache under the name 'posts.all' and remember it until manually cleared
+            return collect(File::files(resource_path("posts")))  //Using the File model to define files, build a collection of all of the files in the "resources/posts" folder
+                ->map(fn($file) => YamlFrontMatter::parseFile($file))   //Map that collection to individual files defined with "file" variable and pass that variable to YamlFrontMatter model to have metadata parsed
+                ->map(fn($document) => new Post(  //Map metadata into documents defined by "document" variable, pass "document" to Post class to give names and/or functions (below) to metadata
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');  //sort posts by date, newest first
+        });
+    }
+
+    public static function find($slug)  //start finding a post based on the given URI defined by "slug" variable
+    {
+        return static::all()->firstWhere('slug', $slug); //out of all the posts (defined above, line 31), display the first one where the "slug" metatag equals the given URI
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts');
-});
-Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    if (!file_exists($path)) {
-        return redirect("/");
-    }
-    $post = file_get_contents($path);
-    return view('post', [
-        'post' => $post
+Route::get('/', function () {  //Answer call to root directory and start working
+    return view('posts', [  //Display /views/posts.blade.php
+        'posts' => Post::all()  //Use the Post model to find all the posts and pass them along to /views/posts.blade.php
     ]);
 });
+
+
+Route::get('posts/{post}', function ($slug) {   //Answer call to posts directory, pass wildcard URI to "slug" variable, get to work
+    return view('post', [   //display /views/post.blade.php
+        'post' => Post::find($slug)   //Using the Post model, find the correct post based on the "slug" variable to pass along to /view/posts.blade.php
+    ]);
+})->where('post', '[A-z_/-]+');  //Only do any of the above if the wildcard URI is composed of letters, underscores, and/or dashes
