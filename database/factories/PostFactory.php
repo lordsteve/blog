64c6c1d1b45665683f53ec0a62ad8188a5444a8f
Steve <live.remix@gmail.com>
2021-12-04 10:02:32 -0500

    taught seeder to give posts thumbnails from given selection

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Storage;
 
 class PostFactory extends Factory
 {
@@ -12,8 +13,12 @@ class PostFactory extends Factory
      *
      * @return array
      */
+
     public function definition()
     {
+        $files = Storage::allFiles('thumbnails');
+        $randomPic = $files[rand(0, count($files) - 1)];
+
         $user = DB::table('users')->inRandomOrder()->first();
         $cat = DB::table('categories')->inRandomOrder()->first();
 
@@ -21,7 +26,7 @@ class PostFactory extends Factory
             'user_id' => $user->id,
             'category_id' => $cat->id,
             'title' => $this->faker->sentence,
-            'thumbnail' => $this->faker->imageUrl,
+            'thumbnail' => $randomPic,
             'slug' => $this->faker->slug,
             'excerpt' => '<p>' . implode('<p></p>', $this->faker->paragraphs(2)) . '</p>',
             'body' => '<p>' . implode('<p></p>', $this->faker->paragraphs(6)) . '</p>',
