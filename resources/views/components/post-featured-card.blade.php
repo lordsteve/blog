1e738e014aeddfc5784c5a19abf13b2e0978432f
Steve <live.remix@gmail.com>
2021-12-04 01:51:47 -0500

    Replaced hard-coded images with images from upload file system or database

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -4,7 +4,11 @@
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
-            <img src="/images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
+            @if (file_exists(public_path('storage/' . $post->thumbnail)))
+                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl">
+            @else
+                <img src="{{ $post->thumbnail }}" alt="" class="rounded-xl">
+            @endif
         </div>
 
         <div class="flex-1 flex flex-col justify-between">
