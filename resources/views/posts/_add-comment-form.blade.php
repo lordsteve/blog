151ea4ce10601438773a9e93907f981d16b5e008
Steve <live.remix@gmail.com>
2021-12-04 16:04:16 -0500

    Fixed comment form with a better understanding of tailwind

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -1,24 +1,19 @@
 @auth
-    <x-panel class="d-flex align-content-center flex-wrap">
-
-        <div class="float-left">
+    <x-panel class="flex">
+        <div class="flex-shrink-0">
             <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-full">
         </div>
-        <div x-data="{ sz: '1', sh : false }" class="d-flex  mt-4 float-right" style="width:calc(100% - 66px)" @click.away="sz = 1, sh = false">
+        <div x-data="{ sz: '1', sh : false }" class="flex-1 flex-shrink-0 ml-4 m-auto" @click.away="sz = 1, sh = false">
             <form name="com" method="POST" action="/post/{{ $post->slug }}/comments">
                 @csrf
-                    <textarea class="txt-sm focus:outline-none" style="width:100%;resize:none"
-                        name="body"
-                        :rows="sz"
-                        placeholder="Go on! Leave a comment!"
-                        required
-                        @click="sz = 10, sh = true"
-                        @keyup.enter="document.com.submit()"></textarea>
-                    <x-form.error name='body' />
-                    <div style="clear: both;"></div>
-                    <div class="flex justify-end mt-6" x-show="sh">
-                        <x-form.button>Post</x-form.button>
-                    </div>
+                <textarea class="txt-sm focus:outline-none w-full" name="body" :rows="sz"
+                    placeholder="Go on! Leave a comment!" required @click="sz = 10, sh = true"
+                    @keyup.enter.prevent="document.com.submit()"></textarea>
+                <x-form.error name='body' />
+                <div style="clear: both;"></div>
+                <div class="flex justify-end" x-show="sh">
+                    <x-form.button>Post</x-form.button>
+                </div>
             </form>
         </div>
         <div style="clear: both;"></div>
@@ -26,7 +21,8 @@
 @else
     <x-panel>
         <p class="font-semibold">
-            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to comment.
+            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to
+            comment.
         </p>
     </x-panel>
 @endauth
