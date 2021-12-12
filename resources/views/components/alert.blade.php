8179c0c4382fe774237916f34afd532209a2b281
Steve Beaudry <live.remix@gmail.com>
2021-12-11 23:04:10 -0500

    removed "grow" from alert buttons, addressing what appeared to be a compatibility issue between Chromebook and Mac also included more robust installation resources for tailwind

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -5,7 +5,7 @@
             class="z-50 filter-none inset-0 w-96 bg-gradient-to-br from-indigo-50 via-white to-white border border-gray-800 m-auto rounded-xl py-auto p-4 text-center flex flex-wrap shadow-lg">
             <span class="center font-semibold mx-auto py-4">{{ session('alert.message') }}</span>
             <button x-on:click="open = false" {{ session('alert.blue-action') }}
-                class="bg-blue-200 py-1 m-auto grow border border-blue-800 rounded-full text-blue-800 bg-gradient-to-tl from-blue-300 via-transparent to-transparent"
+                class="bg-blue-200 py-1 m-auto border border-blue-800 rounded-full text-blue-800 bg-gradient-to-tl from-blue-300 via-transparent to-transparent"
                 :class="pokedyes
                     ? 'px-10 shadow-none'
                     : 'px-11 shadow-lg'"
@@ -15,7 +15,7 @@
                 x-on:mouseup="pokedyes = false"
             >{{ session('alert.blue') }}</button>
             <button x-on:click="open = false" {{ session('alert.red-action') }}
-                class="bg-red-200 py-1 grow m-auto border border-red-800 rounded-full text-red-800 bg-gradient-to-tl from-red-300 via-transparent to-transparent"
+                class="bg-red-200 py-1 m-auto border border-red-800 rounded-full text-red-800 bg-gradient-to-tl from-red-300 via-transparent to-transparent"
                 :class="pokedno
                     ? 'px-10 shadow-none'
                     : 'px-11 shadow-lg'"
