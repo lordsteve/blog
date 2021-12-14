4852d3b20d93bb08551afe9ad92a0188e5ca7dfa
Steve Beaudry <live.remix@gmail.com>
2021-12-14 17:18:59 -0500

    cleaned up post card and fixed issue where Read More was not aligned with the bottom

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,54 +1,48 @@
 @props(['post'])
 
 <article
-    {{ $attributes->merge(['class' => 'fade-in transition-colors duration-300 hover:bg-gray-100 rounded-xl hover:shadow-lg']); }}>
-    <div class="px-5 py-6">
-        <div>
-            <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl">
-        </div>
+    {{ $attributes->merge(['class' => 'flex flex-col justify-between px-5 py-6 fade-in transition-colors duration-300 hover:bg-gray-100 rounded-xl hover:shadow-lg']); }}>
 
-        <div class="flex flex-col justify-between mt-8">
-            <header>
-                <div class="space-x-2">
-                    <x-category-button :category="$post->category"/>
-                </div>
-
-                <div class="mt-4">
-                    @admin
-                        <x-edit-button :id="$post->id" />
-                    @endadmin
-                    <h1 class="text-3xl">
-                        <a href="/posts/{{ $post->slug }}">
-                            {{ $post->title }}
-                        </a>
-                    </h1>
-
-                    <span class="block mt-2 text-xs text-gray-400">
-                        Published <time>{{ $post->created_at->diffForHumans() }}</time>
-                    </span>
-                </div>
-            </header>
-
-            <div class="mt-4 text-sm">
-                {!! $post->excerpt !!}
+        <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl">
+        
+        <header class="mt-8">
+            <div class="space-x-2">
+                <x-category-button :category="$post->category"/>
             </div>
 
-            <footer class="flex items-center justify-between mt-8">
-                <div class="flex items-center text-sm">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
-                    <div class="ml-3">
-                        <h5 class="font-bold">
-                            <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
-                        </h5>
-                    </div>
-                </div>
+            <div class="mt-4">
+                @admin
+                    <x-edit-button :id="$post->id" />
+                @endadmin
+                <h1 class="text-3xl">
+                    <a href="/posts/{{ $post->slug }}">
+                        {{ $post->title }}
+                    </a>
+                </h1>
 
-                <div style="width:130px">
-                    <a href="/posts/{{ $post->slug }}"
-                        class="inline-flex px-8 py-2 text-xs font-semibold transition-colors duration-300 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 active:shadow-none active:scale-95 active:translate-y-1"
-                    >Read More</a>
-                </div>
-            </footer>
+                <span class="block mt-2 text-xs text-gray-400">
+                    Published <time>{{ $post->created_at->diffForHumans() }}</time>
+                </span>
+            </div>
+        </header>
+
+        <div class="mt-4 text-sm">
+            {!! $post->excerpt !!}
         </div>
-    </div>
+
+        <footer class="flex items-center justify-between mt-8">
+            <div class="flex items-center text-sm">
+                <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                <div class="ml-3">
+                    <h5 class="font-bold">
+                        <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
+                    </h5>
+                </div>
+            </div>
+
+                <a href="/posts/{{ $post->slug }}"
+                    class="inline-flex px-8 py-2 text-xs font-semibold transition-colors duration-300 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 active:shadow-none active:scale-95 active:translate-y-1"
+                >Read More</a>
+        </footer>
+
 </article>
