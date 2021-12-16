7a984c228a987202c77b4c047ff073415fdfbf2e
Steve <live.remix@gmail.com>
2021-12-16 15:26:14 -0500

    Found how to append instead of load all on infinite scroll
    WIP: Apparently it means breaking the array because Eloquent models can't be passed??? idk...

--- app/Http/Livewire/PostsGrid.php
+++ app/Http/Livewire/PostsGrid.php
@@ -3,23 +3,44 @@
 namespace App\Http\Livewire;
 
 use App\Models\Post;
+use Illuminate\Pagination\Cursor;
+use Illuminate\Support\Collection;
 use Livewire\Component;
 
 class PostsGrid extends Component
 {
-    public $perPage = 5;
+    public $posts;
+
+    public $nextCursor;
+
+    public $hasMorePages;
+
+    public function mount()
+    {
+        $this->posts = new Collection();
+        $this->loadMore();
+    }
 
     public function loadMore()
     {
-        $this->perPage += 5;
+        if ($this->hasMorePages !== null && !$this->hasMorePages) {
+            return;
+        }
+        $posts = Post::latest()->where('state', 'pub')->filter(request(['search', 'category', 'author']))->cursorPaginate(
+            5,
+            ['*'],
+            'cursor',
+            Cursor::fromEncoded($this->nextCursor)
+        );
+        $this->posts->push(...$posts->items());
+        $this->hasMorePages = $posts->hasMorePages();
+        if ($this->hasMorePages === true) {
+            $this->nextCursor = $posts->nextCursor()->encode();
+        }
     }
 
     public function render()
     {
-        $posts = Post::latest()->where('state', 'pub')->filter(request(['search', 'category', 'author']))->paginate($this->perPage)->withQueryString();
-
-        return view('livewire.posts-grid', [
-            'posts' => $posts
-        ]);
+        return view('livewire.posts-grid');
     }
 }
--- resources/views/livewire/posts-grid.blade.php
+++ resources/views/livewire/posts-grid.blade.php
@@ -1,28 +1,28 @@
 <div class="lg:grid lg:grid-cols-6">
-    @foreach ($posts->skip(1) as $post)
-        <x-post-card :post="$post" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"/>
+    @foreach($posts as $post)
+        <x-post-card :post="$post" :key="$post['id']" class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"/>
     @endforeach
     
-    @if ($posts->hasMorePages())
+    @if ($hasMorePages)
         <button wire:click.prevent='loadMore' class='col-span-2 transition-colors duration-300 py-60 fade-in hover:bg-gray-100 rounded-xl hover:shadow-lg'>Load More</button>
-    @endif
     
-    <div
-        x-data="{
-            observe () {
-                let observer = new IntersectionObserver((entries) => {
-                    entries.forEach(entry => {
-                        if (entry.isIntersecting) {
-                            @this.call('loadMore')
-                        }
+        <div
+            x-data="{
+                observe () {
+                    let observer = new IntersectionObserver((entries) => {
+                        entries.forEach(entry => {
+                            if (entry.isIntersecting) {
+                                @this.call('loadMore')
+                            }
+                        })
+                    }, {
+                        root: null
                     })
-                }, {
-                    root: null
-                })
 
-                observer.observe(this.$el)
-            }
-        }"
-        x-init="observe">
-    </div>
+                    observer.observe(this.$el)
+                }
+            }"
+            x-init="observe">
+        </div>
+    @endif
 </div>
